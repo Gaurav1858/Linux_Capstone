1c4ac090ca79c41b636f2c322d43700501bfbbbc
Gaurav1858 <124980123+Gaurav1858@users.noreply.github.com>
2023-03-13 17:40:23 +0530

    Update use_of_3_cmd_script.sh

--- Linux-Capstone/use_of_3_cmd_script.sh
+++ Linux-Capstone/use_of_3_cmd_script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Ask the user for a file name
 echo "Please enter the name of the file you want to process:"
 read filename
 echo "Content of the file:"
 cat $filename
 echo "" 
 
-# Using grep to find all lines containing any given word in the file
 echo "Give Pattern to search(using grep command): "
 read pattern
 grep -i "$pattern" $filename
@@ -31,17 +29,15 @@ echo "enter filename: "
 read filename
 input_file="$filename"
 
-# Asking the user for the word to replace
 read -p "Enter the word to replace: " old_word
-# Ask the user for the replacement word
+
 read -p "Enter the replacement word: " new_word
 
-# Replacing all occurrences of the old word with the new word
 sed -i "s/$old_word/$new_word/g" "$input_file"
 
 echo "Replaced '$old_word' with '$new_word' in '$input_file'"
 echo ""
-# Print the modified file contents
+
 echo "Modified file contents:"
 cat "$input_file"
 
